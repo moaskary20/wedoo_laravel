e54b7a94d7611f505622ba28628a7a6065e37998
moaskary20 <mo.askary@gmail.com>
2025-12-10 23:20:29 +0200

    Add confirmation to test email button

--- wedoo_admin_panel/resources/views/filament/pages/brevo-settings.blade.php
+++ wedoo_admin_panel/resources/views/filament/pages/brevo-settings.blade.php
@@ -3,7 +3,12 @@
         {{ $this->form }}
         
         <div class="flex justify-end gap-4 mt-6">
-            <x-filament::button type="button" color="info" wire:click="testEmail" icon="heroicon-o-paper-airplane">
+            <x-filament::button 
+                type="button" 
+                color="info" 
+                wire:click="testEmail" 
+                wire:confirm="هل تريد إرسال إيميل تجريبي؟"
+                icon="heroicon-o-paper-airplane">
                 اختبار الإيميل
             </x-filament::button>
             <x-filament::button type="submit" color="success" icon="heroicon-o-check">
