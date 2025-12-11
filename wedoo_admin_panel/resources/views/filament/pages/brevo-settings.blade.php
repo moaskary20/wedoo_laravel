0f410c023d1f98187e0bc727a045d6d72d19fa6f
moaskary20 <mo.askary@gmail.com>
2025-12-11 16:21:09 +0200

    Add PHP-FPM troubleshooting guide

--- wedoo_admin_panel/resources/views/filament/pages/brevo-settings.blade.php
+++ wedoo_admin_panel/resources/views/filament/pages/brevo-settings.blade.php
@@ -16,15 +16,5 @@
             </x-filament::button>
         </div>
     </form>
-    
-    @if ($errors->any())
-        <div class="mt-4 p-4 bg-red-50 border border-red-200 rounded">
-            <ul class="list-disc list-inside text-red-600">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 </x-filament-panels::page>
 
